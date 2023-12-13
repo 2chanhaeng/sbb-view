74041d9b5705f25ff3265aafa04159af174dfea2
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-12-13 22:41:49 +0900

    create post answer form

--- app/questions/[id]/page.tsx
+++ app/questions/[id]/page.tsx
@@ -1,5 +1,7 @@
-import { notFound } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
+import { revalidateTag } from "next/cache";
 import { Question } from "@/types/question";
+import { getPostInitFromForm } from "@/utils/form";
 
 export default async function QuestionDetailPage({
   params: { id },
@@ -24,7 +26,20 @@ export default async function QuestionDetailPage({
             <li key={id}>{content}</li>
           ))}
         </ul>
+        <form action={postAnswer.bind(null, id)}>
+          <textarea name="content" />
+          <button type="submit">Submit</button>
+        </form>
       </section>
     </main>
   );
+}
+
+async function postAnswer(id: string, form: FormData) {
+  "use server";
+  const init = getPostInitFromForm(["content"])(form);
+  console.log(init);
+  fetch(`${process.env.API_URL}/answer/create/${id}`, init);
+  revalidateTag("question");
+  redirect(`/questions/${id}`);
 }
